98f72bddb4b211d605057a074f1d9fb97a9f8c34
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-10-15 23:41:18 +0700

    Pertamax

---
+++ src/main/java/id/web/wirama/App.java
@@ -0,0 +1,14 @@
+package id.web.wirama;
+
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.*;
+
+// Awal Riset Jena
+public class App
+{
+    static String personURI    = "http://somewhere/JohnSmith";
+    static String fullName     = "John Smith";
+    public static void main( String[] args )
+    {
+    }
+}
